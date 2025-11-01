c03685a731a548e8d8d02cf11c0e21e80d96241e
Jakob <jakob.limpens@proton.me>
2025-11-01 12:26:59 +0100

    added simple verlet step method and generic quantity Vector2D implementations

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 mod objects;
 mod physics;
 
-use uom::si::f32::{Length, Mass, Ratio};
-use uom::si::length::meter;
-use uom::si::mass::kilogram;
-use uom::si::ratio::ratio;
+use uom::si::{
+    acceleration::meter_per_second_squared,
+    f32::{Acceleration, Length, Mass, Velocity},
+    length::meter,
+    mass::kilogram,
+    velocity::meter_per_second,
+};
 
 use macroquad::prelude::*;
 
-use objects::point::Point;
+use objects::point::{Point, StepType};
 use physics::vector::Vector2D;
 
 #[macroquad::main("Physics")]
 async fn main() {
-    let mut p = Point {
-        pos: Vector2D {
+    let mut p = Point::new(
+        Vector2D {
             x: Length::new::<meter>(100.0),
             y: Length::new::<meter>(100.0),
         },
-        vel: Vector2D {
-            x: Length::new::<meter>(1.0),
-            y: Length::new::<meter>(0.0),
+        Vector2D {
+            x: Velocity::new::<meter_per_second>(1.0),
+            y: Velocity::new::<meter_per_second>(0.0),
         },
-        acc: Vector2D {
-            x: Length::new::<meter>(0.0),
-            y: Length::new::<meter>(0.01),
+        Vector2D {
+            x: Acceleration::new::<meter_per_second_squared>(0.0),
+            y: Acceleration::new::<meter_per_second_squared>(0.01),
         },
-        mass: Mass::new::<kilogram>(1.0),
-    };
+        Mass::new::<kilogram>(1.0),
+    );
 
     let mass_multiplier: f32 = 20.;
 
@@ -35,7 +38,7 @@ async fn main() {
         clear_background(BLACK);
 
         p.draw_circle(mass_multiplier, WHITE);
-        p.step(None);
+        p.step(Some(StepType::Naive));
 
         next_frame().await;
     }
--- src/physics/vector.rs
+++ src/physics/vector.rs
@@ -3,28 +3,46 @@
 //! This module provides a `Vector2D` struct with basic vector operations
 //! including addition, subtraction, scalar multiplication, and magnitude calculation.
 
-use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
-use uom::si::f32::{Length, Ratio};
+use std::ops::{Add, AddAssign, Div, Mul, Sub, SubAssign};
 
-/// A 2D vector with x and y components.
+/// A generic-typed 2D vector with x and y components.
 #[derive(Debug, Clone, Copy)]
-pub struct Vector2D {
-    pub x: Length,
-    pub y: Length,
+pub struct Vector2D<Q> {
+    pub x: Q,
+    pub y: Q,
 }
 
-impl Vector2D {
-    /// Returns the magnitude (length) of the vector.
-    pub fn mag(&self) -> Length {
+impl<Q> Vector2D<Q>
+where
+    Q: Copy + uom::num_traits::Zero,
+{
+    /// Initialise a zero-vector
+    pub fn zero() -> Self {
+        Self {
+            x: Q::zero(),
+            y: Q::zero(),
+        }
+    }
+}
+
+impl<Q> Vector2D<Q>
+where
+    Q: Copy + Mul<Q, Output = Q> + Add<Output = Q> + uom::num_traits::Float,
+{
+    /// Returns the magnitude of the vector.
+    pub fn mag(&self) -> Q {
         (self.x * self.x + self.y * self.y).sqrt()
     }
 }
 
 /// Implements vector addition for `Vector2D`.
-impl Add for Vector2D {
-    type Output = Vector2D;
+impl<Q> Add for Vector2D<Q>
+where
+    Q: Add<Output = Q> + Copy,
+{
+    type Output = Vector2D<Q>;
 
-    fn add(self, other: Vector2D) -> Vector2D {
+    fn add(self, other: Vector2D<Q>) -> Vector2D<Q> {
         Vector2D {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -33,20 +51,24 @@ impl Add for Vector2D {
 }
 
 /// Implements in-place vector addition for `Vector2D`.
-impl AddAssign for Vector2D {
+impl<Q> AddAssign for Vector2D<Q>
+where
+    Q: AddAssign + Copy,
+{
     fn add_assign(&mut self, other: Self) {
-        *self = Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        };
+        self.x += other.x;
+        self.y += other.y;
     }
 }
 
 /// Implements vector subtraction for `Vector2D`.
-impl Sub for Vector2D {
-    type Output = Vector2D;
+impl<Q> Sub for Vector2D<Q>
+where
+    Q: Sub<Output = Q> + Copy,
+{
+    type Output = Vector2D<Q>;
 
-    fn sub(self, other: Vector2D) -> Vector2D {
+    fn sub(self, other: Vector2D<Q>) -> Vector2D<Q> {
         Vector2D {
             x: self.x - other.x,
             y: self.y - other.y,
@@ -55,33 +77,74 @@ impl Sub for Vector2D {
 }
 
 /// Implements in-place vector subtraction for `Vector2D`.
-impl SubAssign for Vector2D {
+impl<Q> SubAssign for Vector2D<Q>
+where
+    Q: SubAssign + Copy,
+{
     fn sub_assign(&mut self, other: Self) {
-        *self = Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        };
+        self.x -= other.x;
+        self.y -= other.y;
     }
 }
 
-/// Implements scalar multiplication for `Vector2D`.
-impl Mul<Ratio> for Vector2D {
-    type Output = Vector2D;
+/// Implements generic scalar multiplication with `uom::Quantity` for `Vector2D`
+impl<Q, S, Out> Mul<S> for Vector2D<Q>
+where
+    Q: Mul<S, Output = Out> + Copy,
+    S: Copy,
+{
+    type Output = Vector2D<Out>;
 
-    fn mul(self, scalar: Ratio) -> Vector2D {
+    fn mul(self, rhs: S) -> Vector2D<Out> {
         Vector2D {
-            x: self.x * scalar,
-            y: self.y * scalar,
+            x: self.x * rhs,
+            y: self.y * rhs,
         }
     }
 }
 
-/// Implements in-place scalar multiplication for `Vector2D`.
-impl MulAssign<Ratio> for Vector2D {
-    fn mul_assign(&mut self, scalar: Ratio) {
-        *self = Self {
-            x: self.x * scalar,
-            y: self.y * scalar,
-        };
+/// Implements division by generic type for `Vector2D<Q>`
+impl<Q, S, Out> Div<S> for Vector2D<Q>
+where
+    Q: Div<S, Output = Out> + Copy,
+    S: Copy,
+{
+    type Output = Vector2D<Out>;
+
+    fn div(self, rhs: S) -> Vector2D<Out> {
+        Vector2D {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
     }
 }
+
+/// Due to Rust orphan rule, we cannot implement multiplicatin with `Vector2D` for generic quantity.
+/// To sort-of make this scalable, we use a macro to implement multiplication for specific types.
+macro_rules! impl_vector_mul {
+    ($quantity:ty) => {
+        impl<Q, Out> Mul<Vector2D<Q>> for $quantity
+        where
+            Q: Mul<$quantity, Output = Out> + Copy,
+        {
+            type Output = Vector2D<Out>;
+
+            fn mul(self, rhs: Vector2D<Q>) -> Vector2D<Out> {
+                Vector2D {
+                    x: rhs.x * self,
+                    y: rhs.y * self,
+                }
+            }
+        }
+    };
+}
+
+// Add more quantities here as needed
+use uom::si::f32::{Acceleration, Force, Length, Mass, Ratio, Time, Velocity};
+impl_vector_mul!(Acceleration);
+impl_vector_mul!(Force);
+impl_vector_mul!(Length);
+impl_vector_mul!(Mass);
+impl_vector_mul!(Ratio);
+impl_vector_mul!(Time);
+impl_vector_mul!(Velocity);
