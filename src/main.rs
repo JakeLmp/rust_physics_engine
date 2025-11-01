8f96bbc5ae8d112437afb7444e3471399c396642
Jakob <jakob.limpens@proton.me>
2025-11-01 17:30:04 +0100

    vec of points, verlet step method

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,35 @@ use physics::vector::Vector2D;
 
 #[macroquad::main("Physics")]
 async fn main() {
-    let mut p = Point::new(
-        Vector2D {
-            x: Length::new::<meter>(100.0),
-            y: Length::new::<meter>(100.0),
-        },
-        Vector2D {
-            x: Velocity::new::<meter_per_second>(1.0),
-            y: Velocity::new::<meter_per_second>(0.0),
-        },
-        Vector2D {
-            x: Acceleration::new::<meter_per_second_squared>(0.0),
-            y: Acceleration::new::<meter_per_second_squared>(0.01),
-        },
-        Mass::new::<kilogram>(1.0),
-    );
+    let mut points: Vec<Point> = Vec::new();
+    for _i in 0..10 {
+        points.push(Point::new(
+            Vector2D {
+                x: Length::new::<meter>(rand::gen_range(0.0, 800.0)),
+                y: Length::new::<meter>(rand::gen_range(0.0, 600.0)),
+            },
+            Vector2D {
+                x: Velocity::new::<meter_per_second>(0.0),
+                y: Velocity::new::<meter_per_second>(0.0),
+            },
+            Vector2D {
+                x: Acceleration::new::<meter_per_second_squared>(rand::gen_range(-1.0, 1.0)),
+                y: Acceleration::new::<meter_per_second_squared>(rand::gen_range(-1.0, 1.0)),
+            },
+            Mass::new::<kilogram>(1.0),
+        ));
+    }
 
     let mass_multiplier: f32 = 20.;
+    let color = WHITE;
 
     loop {
         clear_background(BLACK);
 
-        p.draw_circle(mass_multiplier, WHITE);
-        p.step(Some(StepType::Naive));
+        for p in points.iter_mut() {
+            p.draw_circle(mass_multiplier, color);
+            p.step(Some(StepType::Verlet));
+        }
 
         next_frame().await;
     }
